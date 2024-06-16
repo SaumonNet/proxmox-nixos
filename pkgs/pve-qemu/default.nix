3c98f75fb3f117acf9c29590b2f88430ec4828f5
Julien Malka <julien@malka.sh>
2024-06-16 11:33:09 +0200

    fix pve-qemu

--- pkgs/pve-qemu/default.nix
+++ pkgs/pve-qemu/default.nix
@@ -2,6 +2,10 @@
   qemu,
   fetchgit,
   fetchurl,
+  proxmox-backup-qemu,
+  perl,
+  pkg-config,
+  ...
 }:
 
 (
@@ -13,6 +17,21 @@
       hash = "sha256-l2Ox7+xP1JeWtQgNCINRLXDLY4nq1lxmHMNoalIjKJY=";
     };
 
+    buildInputs = old.buildInputs ++ [ proxmox-backup-qemu ];
+    propagatedBuildInputs = [ proxmox-backup-qemu ];
+    preBuild =
+      ''
+        cp ${proxmox-backup-qemu}/lib/proxmox-backup-qemu.h .
+
+      ''
+      + old.preBuild;
+
+    nativeBuildInputs = old.nativeBuildInputs ++ [
+      proxmox-backup-qemu
+      perl
+      pkg-config
+    ];
+
     patches =
       let
         src_patches = fetchgit {
@@ -23,14 +42,17 @@
         };
         patchesDir = "${src_patches}/debian/patches";
       in
-      [ (builtins.head old.patches) ]
+      [ old.patches ]
       ++ map (e: "${patchesDir}/extra/${e}") (
         builtins.attrNames (builtins.readDir "${patchesDir}/extra/")
       )
-      ++ map (e: "${patchesDir}/pve/${e}") (builtins.attrNames (builtins.readDir "${patchesDir}/pve/"))
       ++ map (e: "${patchesDir}/bitmap-mirror/${e}") (
         builtins.attrNames (builtins.readDir "${patchesDir}/bitmap-mirror/")
-      );
+      )
+      ++ map (e: "${patchesDir}/pve/${e}") (builtins.attrNames (builtins.readDir "${patchesDir}/pve/"));
   })).override
-  { glusterfsSupport = true; }
+  {
+    glusterfsSupport = true;
+    enableDocs = false;
+  }
 )

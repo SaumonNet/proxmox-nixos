da2cef080b494209624a5e296e0bea78897d5ad8
Julien Malka <julien@malka.sh>
2024-06-17 22:44:46 +0200

    init proxmox networking setup

--- flake.nix
+++ flake.nix
@@ -63,7 +63,9 @@
             {
               networking = {
                 hostName = "proxmox-dev";
-                networkmanager.enable = true;
+
+                useNetworkd = true;
+
                 firewall.allowedTCPPorts = [
                   80
                   443
@@ -80,6 +82,29 @@
                 ];
                 nftables.enable = true;
               };
+              systemd.network.networks."10-lan" = {
+                matchConfig.Name = [ "ens18" ];
+                networkConfig = {
+                  Bridge = "vmbr0";
+                };
+              };
+
+              systemd.network.netdevs."vmbr0" = {
+                netdevConfig = {
+                  Name = "vmbr0";
+                  Kind = "bridge";
+                };
+              };
+
+              systemd.network.networks."10-lan-bridge" = {
+                matchConfig.Name = "vmbr0";
+                networkConfig = {
+                  IPv6AcceptRA = true;
+                  DHCP = "ipv4";
+                };
+                linkConfig.RequiredForOnline = "routable";
+              };
+
               boot.specialFileSystems."/dev/pts" = {
                 fsType = "devpts";
                 options = [

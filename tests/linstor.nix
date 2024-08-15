88788b7a9aea81b93497a50cb2a6e305a949d2bd
Camille M. (genesis) <camillemondon@free.fr>
2024-08-15 19:19:54 +0200

    fix: increase ram for linstor test

--- tests/linstor.nix
+++ tests/linstor.nix
@@ -8,12 +8,14 @@
         enable = true;
         linstor.enable = true;
       };
+      virtualisation.memorySize = 2048;
     };
     pve2 = {
       services.proxmox-ve = {
         enable = true;
         linstor.enable = true;
       };
+      virtualisation.memorySize = 2048;
     };
   };
 
@@ -21,6 +23,7 @@
     pve1.start()
     pve2.start()
     pve1.wait_for_unit("pveproxy.service")
+    pve1.wait_for_unit("linstor-controller.service")
     assert "running" in pve1.succeed("pveproxy status")
     assert "Proxmox" in pve1.succeed("curl -k https://localhost:8006")
     pve1.succeed("pvecm create mycluster")

89a8f0f915dc8fe0022ee95c57268b915af8671f
Julien Malka <julien@malka.sh>
2024-08-31 11:21:10 +0200

    chore: arbitrary machine name

--- pkgs/nixmoxer/eval-non-flake.nix
+++ pkgs/nixmoxer/eval-non-flake.nix
@@ -1,10 +1,11 @@
 let
+  machine = "placeholder";
   conf = import "placeholder_config";
   isoConfig = import ./iso.nix;
   toplevel = conf.nixosConfigurations.vm-dev.config.system.build;
 in
 
-(import "${conf.nixosConfigurations.vm-dev.pkgs.path}/nixos/lib/eval-config.nix" {
+(import "${conf.nixosConfigurations.${machine}.pkgs.path}/nixos/lib/eval-config.nix" {
   system = "x86_64-linux";
   modules = [ (isoConfig toplevel) ];
 }).config.system.build.isoImage

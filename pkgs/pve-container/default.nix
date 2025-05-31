0e6edd9168afc540ccbacce47794b9508078605b
Thomas Ieong <th.ieong@free.fr>
2025-05-31 23:14:35 +0200

    pve-container: Added missings deps and fixed harcoded paths

--- pkgs/pve-container/default.nix
+++ pkgs/pve-container/default.nix
@@ -5,12 +5,33 @@
   perl538,
   dtach,
   lxc,
+  lxcfs,
   openssh,
+  pve-common,
+  binutils,
+  shadow,
+  pve-cluster,
+  pve-firewall,
+  gnutar,
+  util-linux,
+  iproute2,
+  termproxy,
+  vncterm,
+  xz,
+  pve-storage,
+  pve-guest-common,
+  e2fsprogs,
   tzdata,
 }:
 
 let
-  perlDeps = [ ];
+  perlDeps = [
+    pve-guest-common
+    pve-common
+    pve-cluster
+    pve-storage
+    pve-firewall
+  ];
   perlEnv = perl538.withPackages (_: perlDeps);
 in
 
@@ -65,11 +86,65 @@ perl538.pkgs.toPerlModule (
       find $out -type f | xargs sed -i \
         -e "s|/usr/bin/dtach|${dtach}/bin/dtach|" \
         -e "s|/usr/bin/ssh|${openssh}/bin/ssh|" \
-        -e "s|/usr/bin/vncterm||" \
-        -e "s|/usr/bin/termproxy||" \
+        -e "s|mkfs.ext4|${e2fsprogs}/bin/mkfs.ext4|" \
+        -e "s|lxc-start --version|${lxc}/bin/lxc-start --version|" \
+        -e "s|/sbin/ip|${iproute2}/bin/ip|" \
+        -e "s|'umount'|'${util-linux}/bin/umount'|" \
+        -e "s|'/bin/umount'|'${util-linux}/bin/umount'|" \
+        -e "s|@lxcfspath@|${lxcfs}|" \
+        -e "s|@lxcpath@|${lxc}|" \
+        -e 's|$LXC_CONFIG_PATH/common.seccomp|${lxc}/share/lxc/config/common.seccomp|' \
+        -e "s|$cfgpath/userns.conf|$cfgpath/userns.conf.d/01-pve.conf|" \
+        -e "s|lxc-console|${lxc}/bin/lxc-console|" \
+        -e "s|lxc-device|${lxc}/bin/lxc-device|" \
+        -e "s|lxc-attach|${lxc}/bin/lxc-attach|" \
+        -e "s|lxc-stop|${lxc}/bin/lxc-stop|" \
+        -e "s|lxc-info|${lxc}/bin/lxc-info|" \
+        -e "s|'lxc-usernsexec'|'${lxc}/bin/lxc-usernsexec'|" \
+        -e "s|'losetup'|'${util-linux}/bin/losetup'|" \
+        -e "s|/usr/bin/vncterm|${vncterm}/bin/vncterm|" \
+        -e "s|/usr/bin/termproxy|${termproxy}/bin/termproxy|" \
+        -e "s|'objdump'|'${binutils}/bin/objdump'|" \
+        -e "s|'newgidmap'|'${shadow}/bin/newgidmap'|" \
+        -e "s|'newuidmap'|'${shadow}/bin/newuidmap'|" \
+        -e "s|'nsenter'|'${util-linux}/bin/nsenter'|" \
         -e "s|/usr/bin/lxc|${lxc}/bin/lxc|" \
         -e "s|/usr/share/lxc|$out/share/lxc|" \
         -e "s|/usr/share/zoneinfo|${tzdata}/share/zoneinfo|"
+
+      sed -i $out/lib/perl5/site_perl/5.38.2/PVE/LXC/Create.pm \
+        -e "s|'tar'|'${gnutar}/bin/tar'|"
+
+      sed -i $out/lib/perl5/site_perl/5.38.2/PVE/LXC/Create.pm \
+        -e "s|$mechanism eq '${gnutar}'|$mechanism eq 'tar'|"
+
+      sed -i $out/lib/perl5/site_perl/5.38.2/PVE/LXC.pm \
+        -e "s|'mount'|'${util-linux}/bin/mount'|" \
+
+      sed -i $out/lib/perl5/site_perl/5.38.2/PVE/VZDump/LXC.pm \
+        -e "s|'mount'|'${util-linux}/bin/mount'|" \
+
+      patchShebangs $out/share/lxc/lxcnetaddbr
+      patchShebangs $out/share/lxc/pve-container-stop-wrapper
+      patchShebangs $out/share/lxc/hooks/lxc-pve-autodev-hook
+      patchShebangs $out/share/lxc/hooks/lxc-pve-prestart-hook
+      patchShebangs $out/share/lxc/hooks/lxc-pve-poststop-hook
+
+      sed -i $out/share/lxc/hooks/lxc-pve-prestart-hook \
+        -e "s/-T//" \
+        -e "1s|$| -I$out/${perl538.libPrefix}/${perl538.version}|"
+
+      sed -i $out/share/lxc/hooks/lxc-pve-autodev-hook \
+        -e "s/-T//" \
+        -e "1s|$| -I$out/${perl538.libPrefix}/${perl538.version}|"
+
+     sed -i $out/share/lxc/hooks/lxc-pve-poststop-hook \
+        -e "s/-T//" \
+        -e "1s|$| -I$out/${perl538.libPrefix}/${perl538.version}|"
+
+     sed -i $out/share/lxc/lxcnetaddbr \
+        -e "s/-T//" \
+        -e "1s|$| -I$out/${perl538.libPrefix}/${perl538.version}|"
     '';
 
     passthru.updateScript = [

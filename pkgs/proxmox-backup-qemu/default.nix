c475013fb54eac465a9ba94f0114a9778764afba
Viatcheslav Soloviev <fuse.to.defuse@gmail.com>
2025-06-05 04:35:44 +0300

    Revert "Update default.nix"
    This reverts commit af4dd61de4b70c64d9ac88c9b31328a3138c3add.

--- pkgs/proxmox-backup-qemu/default.nix
+++ pkgs/proxmox-backup-qemu/default.nix
@@ -26,7 +26,7 @@ rustPlatform.buildRustPackage rec {
   src = fetchgit {
     url = "git://git.proxmox.com/git/${pname}.git";
     rev = "c3cbcae289d04b4454a70fc59dc58a19d5edb681";
-    hash = "sha256-NjW6uv96Bz7tPOFDnAtDfFFP+lruRZ2OZeYhxz6LVtE=";
+    hash = "sha256-rbKKcPwswZoIAFGdW9SxG2goHC4U8TG0pYQl1M6jku0=";
     fetchSubmodules = true;
     leaveDotGit = true;
   };

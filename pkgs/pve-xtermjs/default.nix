fa67c9ba2dd7908b3918540ad9a770fc5622f44f
codgician <15964984+codgician@users.noreply.github.com>
2026-01-29 15:42:54 +0800

    pve-xtermjs: 5.5.0-2 -> 5.5.0-3

--- pkgs/pve-xtermjs/default.nix
+++ pkgs/pve-xtermjs/default.nix
@@ -5,14 +5,14 @@
   pve-update-script,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   pname = "pve-xtermjs";
-  version = "5.5.0-2";
+  version = "5.5.0-3";
 
   src = fetchgit {
     url = "git://git.proxmox.com/git/pve-xtermjs.git";
-    rev = "a29b36079fbaf18586615e26bb615992d1007c7e";
-    hash = "sha256-89K93D8uD1pAynM1M3ixbbZS3p7Sxga9OA/HgpVBeHY=";
+    rev = "222b38aa8f226146d236a7f5f82744d03a8557df";
+    hash = "sha256-OunLO3sGkpF7nbB0pNP4zTcxT1xHR7j/J0ZaV7UJhug=";
   };
 
   dontBuild = true;

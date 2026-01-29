374c7cea73676ea5620ae7723c3fd14d6664e5dc
codgician <15964984+codgician@users.noreply.github.com>
2026-01-29 16:02:24 +0800

    proxmox-widget-toolkit: 5.1.4 -> 5.1.5

--- pkgs/proxmox-widget-toolkit/default.nix
+++ pkgs/proxmox-widget-toolkit/default.nix
@@ -10,12 +10,12 @@
 
 stdenv.mkDerivation rec {
   pname = "proxmox-widget-toolkit";
-  version = "5.1.4";
+  version = "5.1.5";
 
   src = fetchgit {
     url = "git://git.proxmox.com/git/proxmox-widget-toolkit.git";
-    rev = "d38cd3d923f1919556bc67ecff7b95ae20f3052f";
-    hash = "sha256-So3KbPOzsohiwzev+5IArKAR6/PIUQRwo39MxMJPong=";
+    rev = "e0798f947fb727ce57f3423e782465a3c9115cfe";
+    hash = "sha256-wXHVgWnq/HUDJ3O6Jkgk521+AQwd9cLR4StACGO6buc=";
   };
 
   sourceRoot = "${src.name}/src";
